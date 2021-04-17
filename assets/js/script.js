a89dddb891c105b10aac00088d0c13f1fb8e907d
Robert Schwartz <bschwartz23@gmail.com>
2021-04-17 16:22:14 -0700

    set up parse time function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 //adding current date to jumbo
-var now = moment().format("dddd, MMMM, Do YYYY"); 
+var now = moment().format("LLLL");
 $("#currentDay").text(now);
 
 //clear local storage
 // var reset = localStorage.clear();
+
+var compareTime = function(textTime) {
+textTime = textTime + 7;
+var parseTime = "1970-01-01 "+textTime+":00Z";
+var momentTime = moment(parseTime);
+console.log(momentTime.format("H"));
+console.log(momentTime);
+//
+};
+compareTime(09);
+
+
 
 //save buttons function
 $(".saveBtn").on("click", function () {
@@ -24,7 +36,3 @@ $("#hour2").val(localStorage.getItem("hour2"));
 $("#hour3").val(localStorage.getItem("hour3"));
 $("#hour4").val(localStorage.getItem("hour4"));
 $("#hour5").val(localStorage.getItem("hour5"));
-
-
-
-
