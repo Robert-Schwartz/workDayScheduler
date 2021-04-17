ade69b409657943cbaf3a122d47efe65c7a14d97
Robert Schwartz <bschwartz23@gmail.com>
2021-04-17 13:20:25 -0700

    duplicate for each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,23 @@ var now = dayjs();
 $("#currentDay").text(now);
 
 //save button functions
-$(".saveBtn").on("click",function(){
-    var saveTask = $(this).siblings("textarea").val();
-    var taskID = $(this).siblings("textarea").attr("id");
-    console.log(saveTask,taskID);
-    localStorage.setItem(taskID,saveTask);
-})
+$(".saveBtn").on("click", function () {
+  var saveTask = $(this).siblings("textarea").val();
+  var taskID = $(this).siblings("textarea").attr("id");
+  console.log(saveTask, taskID);
+  localStorage.setItem(taskID, saveTask);
+});
 
 //show saved values
-$("#hour9").val(localStorage.getItem("hour9"))
+$("#hour9").val(localStorage.getItem("hour9"));
+$("#hour10").val(localStorage.getItem("hour10"));
+$("#hour11").val(localStorage.getItem("hour11"));
+$("#hour12").val(localStorage.getItem("hour12"));
+$("#hour1").val(localStorage.getItem("hour1"));
+$("#hour2").val(localStorage.getItem("hour2"));
+$("#hour3").val(localStorage.getItem("hour3"));
+$("#hour4").val(localStorage.getItem("hour4"));
+$("#hour5").val(localStorage.getItem("hour5"));
+
+//clear local storage
+// var reset = localStorage.clear();
