894ed242b81744e98d0173775cc1469fb9810c4a
Robert Schwartz <bschwartz23@gmail.com>
2021-04-17 12:43:18 -0700

    add save btn function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
-var now = dayjs(new Date());
+//adding current date to jumbo
+var now = dayjs();
+$("#currentDay").text(now);
 
-console.log(now);
+//save button functions
+$(".saveBtn").on("click",function(){
+    var saveTask = $(this).siblings("textarea").val();
+    var taskID = $(this).siblings("textarea").attr("id");
+    console.log(saveTask,taskID);
+    localStorage.setItem(taskID,saveTask);
+})
+
+//show saved values
+$("#hour9").val(localStorage.getItem("hour9"))
